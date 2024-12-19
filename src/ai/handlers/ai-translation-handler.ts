a2f92cbe3e3d2df29e5fb33cf5bfe82c7f840bf4
lte <lte@bringme.com>
2024-12-19 21:33:05 +0100

    Somewhat working draft version of translation

--- src/ai/handlers/ai-translation-handler.ts
+++ src/ai/handlers/ai-translation-handler.ts
@@ -1,4 +1,6 @@
+import { InternalServerErrorException, Logger } from '@nestjs/common';
 import { AiService, GroceryItem } from '../services/ai.service';
+import { isHiragana, toKatakana, toRomaji } from 'wanakana';
 
 export class AiTranslationCommand {
   inputText: string;
@@ -9,23 +11,55 @@ export class AiTranslationCommand {
 }
 
 export class AiTranslationHandler {
+  private readonly logger = new Logger(AiTranslationHandler.name);
+
   constructor(private aiService: AiService) {}
   async execute({ inputText }: AiTranslationCommand): Promise<GroceryItem> {
     const translation =
       await this.aiService.translateWithHuggingface(inputText);
     const hiragana = await this.getHiragana(translation);
+    const katakana = toKatakana(hiragana);
+    const romaji = toRomaji(hiragana);
 
     return {
       emoji: null,
       id: null,
       nameEnglish: inputText,
       nameHiragana: hiragana,
-      nameKatakana: translation,
-      nameRomaji: 'todo',
+      nameKatakana: katakana,
+      nameRomaji: romaji,
     };
   }
 
   private async getHiragana(inputText: string): Promise<string> {
+    let extractedReponse = await this.tryConvertToHiraganaUsingAi(inputText);
+
+    const maxAttempts = 3;
+    let attempts = 0;
+    let answer = null;
+
+    while (attempts <= maxAttempts) {
+      this.logger.log(extractedReponse);
+
+      if (isHiragana(extractedReponse)) {
+        this.logger.warn('Ok found hiragana run: ' + attempts);
+        answer = extractedReponse;
+        break;
+      } else {
+        this.logger.warn('Hmm isnt hiragana, retrying.. run: ' + attempts);
+        extractedReponse = await this.tryConvertToHiraganaUsingAi(inputText);
+      }
+
+      if (attempts === maxAttempts) {
+        throw new InternalServerErrorException('Cannot translate');
+      }
+      attempts++;
+    }
+
+    return answer;
+  }
+
+  private async tryConvertToHiraganaUsingAi(inputText: string) {
     const prompt = `
 
         you will receive a japanese text and must convert any katakana and kanji characters into hiragana.
@@ -45,12 +79,11 @@ export class AiTranslationHandler {
       'mistralai/Mistral-Nemo-Instruct-2407',
     );
 
-    const hiragana = this.extractLastHiragana(response);
-
-    return hiragana;
+    const extractedReponse = this.extractLastHiragana(response);
+    return extractedReponse;
   }
 
-  extractLastHiragana(response) {
+  extractLastHiragana(response: string) {
     const startTag = '##start response##';
     const endTag = '##end response##';
 
